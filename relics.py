f40e32fa476514d1b9a81d34a1a0bc7c2d17856b
Luís Campos <109487926+SonOfGod314@users.noreply.github.com>
2024-09-26 17:42:34 -0300

    Create relics.py

---
+++ relics.py
@@ -0,0 +1,25 @@
+
+#ritual
+ritual_real_2 = 0.2
+ritual_real_4 = 0.2
+
+#codice_obsidiana
+codice_obsidiana_2 = 0.15
+codice_obsidiana_4 = 0.4
+
+#milelith
+milelith_2 = 0.2
+milelith_4 = 0.2
+
+#reminiscencia_incompleta
+rem_inc_2 = 0.18
+rem_inc_4 = 0.5
+
+#memorias_da_floresta
+mem_flo_2 = 0.15
+mem_flo_4 = 0.3
+
+#pergaminho_do_heroi
+perg_hero_2 = 6
+perg_hero_4_1 = 0.12
+perg_hero_4_2 = 0.4
