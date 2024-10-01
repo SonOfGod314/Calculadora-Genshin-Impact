8b70b329ce69ae86baaba3671ddc46a5ec87f32e
Luís Campos <109487926+SonOfGod314@users.noreply.github.com>
2024-10-01 17:55:56 -0300

    Update weapons.py

--- weapons.py
+++ weapons.py
@@ -1,3 +1,10 @@
 
 #earth shaker
-earthshaker = 0.32
+earthshaker_base = 565
+earthshaker_stat = 0.276
+earthshaker_p1 = 0.32
+
+#serpent spine
+serp_spine_base = 510
+serp_spine_stat = 0.276
+serp_spine_p1 = 0.3
