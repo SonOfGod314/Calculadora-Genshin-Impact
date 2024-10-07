0fd1e72723ec7dbe36935c1084b6b8994b809642
Luís Campos <109487926+SonOfGod314@users.noreply.github.com>
2024-10-07 17:23:44 -0300

    Update weapons.py

--- weapons.py
+++ weapons.py
@@ -1,10 +1,28 @@
 
-#earth shaker
+#earth_shaker_R5
 earthshaker_base = 565
 earthshaker_stat = 0.276
 earthshaker_p1 = 0.32
 
-#serpent spine
+#serpent_spine
 serp_spine_base = 510
 serp_spine_stat = 0.276
 serp_spine_p1 = 0.3
+
+#sacrificio_de_jade
+jade_base = 454
+jade_stat = 0.368
+jade_p1 = 0.32
+jade_p2 = 40
+
+#espada_do_penhasco_obscuro
+blacklif_sword_base = 565
+blacklif_sword_stat = 0.368
+blacklif_sword_p1 = 0.12
+
+#epilogo_das_profundezas_R3
+epi_prof_base = 565
+epi_prof_stat = 0.276
+epi_prof_p1 = 0.18
+epi_prof_vida_considerada = 0.25*20000
+epi_prof_p2 = 0.036*epi_prof_vida_considerada
