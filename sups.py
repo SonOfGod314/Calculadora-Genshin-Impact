63bfa23a507c7b2418244e18d9e71bc00f127de6
Luís Campos <109487926+SonOfGod314@users.noreply.github.com>
2024-10-07 17:22:54 -0300

    Update sups.py

--- sups.py
+++ sups.py
@@ -3,9 +3,15 @@
 benny_ult = 1110
 
 #furina
-fanfarra = 100
+fanfarra = 200
 furina_ult = (0.25*fanfarra)/100
 
 #yelan
 seg_dado = 5
 yelan_passiva = 0.01+(seg_dado*0.035) 
+
+#kazuha
+kazuha_buff = 0.4
+
+#xianyun
+xianyun_ult = 6000
